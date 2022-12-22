b07471dd2cecde34633dffdad97fcc2a251e4d19
HiteshKishoreDas <dashiteshkishore@gmail.com>
2022-12-22 16:03:57 +0100

    Made dark and light html versions

---
+++ html_export.sh
@@ -0,0 +1,2 @@
+jupyter nbconvert --execute --to html --output "deprojection_light" deproj_jupy.ipynb
+jupyter nbconvert --execute --to html --output "deprojection_dark" deproj_jupy.ipynb --HTMLExporter.theme=dark
